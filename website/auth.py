7fe68dabf6f9e27b91774f6e67863588f4e86d51
Jack Chen <jackczy1209@gmail.com>
2021-10-02 20:29:24 -0400

    Finish building sign-up functionality, added column "last_name" to user table

--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,8 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
+
 
 
 auth = Blueprint('auth', __name__) #initialize authentication blueprint
@@ -20,20 +24,31 @@ def signup():  # handles sign up operation
         # get data from form submitted
         email = request.form.get('email')
         firstName = request.form.get('firstName')
+        lastName = request.form.get('lastName')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
 
         if len(email)<4:  # ensure email input is a valid email
             flash('you must have a valid email', category='error')
         elif len(firstName)<2:  # ensure first name is greater than 2 characters
-            flash('you must enter a valid name', category='error')
+            flash('you must enter a valid first name', category='error')
         elif password1 != password2: # ensure that password confirmation is valid
             flash('Passwords don\'t match', category='error')
         elif len(password1)<7:  # ensure that password is secure enough
             flash('password is too weak. Please try again', category='error')
+        elif len(lastName)<2:  # ensure last name is greater than 2 characters
+            flash('you must enter a valid last name', category='error')
         else:
+
+            # create new user/add user to database
+            new_user = User(email=email, first_name=firstName, last_name=lastName, password=generate_password_hash(password=password1, method='sha256' )) # note: password is hashed through SHA256 algorithm
+            db.session.add(new_user)
+            db.session.commit()
+
+            # redirect user to home
             flash('successfully created an account', category='success')
-            # add user to database
-            pass
+            return redirect(url_for('views.home'))
+
+
 
     return render_template("signup.html")
